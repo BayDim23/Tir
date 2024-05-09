f5e0d22eb8a52f7812037f184991678125b802b8
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-05-09 06:05:20 +0300

    add new const

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import pygame
 
 pygame.init()
 
-running = True
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("icon.png")
+
+running = True
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+running = True
 while running:
     pass
 
